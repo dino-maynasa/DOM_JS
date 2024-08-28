da77bb9f6d14a308d3db62d523f8dd9396f0000d
Dino <dino.maynasa.maydana@gmail.com>
2024-08-28 16:09:15 -0400

    asignar estilos

---
+++ toppings-de-pizza/script.js
@@ -0,0 +1,6 @@
+//personalizar los estilos con css
+const primmerTopping =  document.querySelector('.topping');
+primmerTopping.style.backgroundColor = 'blue';
+primmerTopping.style.color = 'yellow';
+primmerTopping.style.textTransform = 'uppercase';
+console.log(primmerTopping);
