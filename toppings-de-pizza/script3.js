6a5a51323bc9601b7ae999fca20bf83b006fe35d
Dino <dino.maynasa.maydana@gmail.com>
2024-08-29 16:39:56 -0400

    agregar una clase

---
+++ toppings-de-pizza/script3.js
@@ -0,0 +1,8 @@
+//agregar clases
+const primmerTopping = document.querySelector('.topping');
+//se agrega con add
+primmerTopping.classList.add('clase-agregado');
+//despliega el listado de las clases
+console.log(primmerTopping.classList);
+//muestra la clase agregada
+console.log(primmerTopping.classList[2]);
