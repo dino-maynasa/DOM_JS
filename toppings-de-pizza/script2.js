c61bc2301c47051a5982dd2c30445dcfe5135aa1
Dino <dino.maynasa.maydana@gmail.com>
2024-08-29 16:19:55 -0400

    clases

---
+++ toppings-de-pizza/script2.js
@@ -0,0 +1,5 @@
+//agregar y modificar clases de elementos
+const  primerTopping = document.querySelector('.topping');
+//classList = lista de clases
+console.log(primerTopping);
+console.log(primerTopping.classList); 
