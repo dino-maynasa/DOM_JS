6fbd840f402352a345471aa983ae4680b1500b78
Dino <dino.maynasa.maydana@gmail.com>
2024-08-30 16:07:20 -0400

    colores aleatorios hexadecimales

---
+++ colores aleatorios/script.js
@@ -0,0 +1,22 @@
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+// generar color hexadecimal aleatorio
+function generarColorAleatorio() {
+//bosibles valores
+let digitos = '0123456789ABCDEF';
+let colorHex = '#';
+
+for (let i = 0; i <6; i++) {
+    let indiceAleatorio=Math.floor( Math.random()*16);
+    colorHex += digitos[indiceAleatorio];
+}
+return colorHex;
+}
+//addEventListener
+boton.addEventListener('click', function() { 
+    let colorAleatorio = generarColorAleatorio();
+    //actualizar el texto
+    color.textContent = colorAleatorio;
+    //actualizar el color de fondo
+    document.body.style.backgroundColor = colorAleatorio;
+});
